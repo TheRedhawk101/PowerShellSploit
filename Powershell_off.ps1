be0870411a56e45da249175d2ee506f740f94b74
TheRedhawk101 <114465486+TheRedhawk101@users.noreply.github.com>
2022-09-27 11:41:06 +0530

    Add files via upload

---
+++ Powershell_off.ps1
@@ -0,0 +1,8 @@
+#You can change the scope to MachinePolicy, UserPolicy, Process, CurrentUser, LocalMachine as per your requirements
+
+#To set the or enable the script execution permission in LocalMachine use bellow script
+Set-ExecutionPolicy -Scope LocalMachine Unrestricted
+
+#To set the or enable the script execution permission in CurrentUser (one in the C:\ drive) use bellow script
+#Set-ExecutionPolicy -Scope CurrentUser Unrestricted
+
